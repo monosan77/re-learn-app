8aa2707cc538f31a89eefefdd902ab0cb7af796f
monosan77 <nogu.y-soccer.1o0706@ezweb.ne.jp>
2024-11-26 16:51:11 +0900

    ロゴとタイトルをコンポーネント化に伴う修正

--- src/app/users/login/page.tsx
+++ src/app/users/login/page.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import PageTitle from "./components/PageTitle";
 import LoginForm from "./components/LoginForm";
+import LogoAndTitle from "@/components/Title/LogoAndTitle";
 
 const Page = () => {
   return (
     <div className="w-full min-w-60 flex justify-center">
       <div className="mt-20">
-        <PageTitle />
+        <LogoAndTitle title="Login" />
         <LoginForm />
       </div>
     </div>
