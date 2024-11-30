c8dcc49801963846663755a2428b715a1ca7f73d
monosan77 <nogu.y-soccer.1o0706@ezweb.ne.jp>
2024-11-30 14:38:56 +0900

    問題作成画面の作成

---
+++ src/components/ProblemForm/ProblemTitle.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ProblemTitle = () => {
+  return (
+    <div className="w-full bg-mainColor">
+      <div className=" text-white px-4 py-5  w-full max-w-1000 mx-auto">
+        <h1 className="font-bold text-xl mb-2">プログラミング</h1>
+        <div className="flex justify-start items-center space-x-4">
+          <h3>NextJs</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProblemTitle;
